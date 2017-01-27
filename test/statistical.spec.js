07e53efbaaa172aac9b2f1811843e743272a2182
AdrienDePeretti <adrien.deperetti.freelance@gmail.com>
2017-01-27 21:34:09 +0100

    Add test on stdDeviation

--- test/statistical.spec.js
+++ test/statistical.spec.js
@@ -51,4 +51,26 @@ describe('Statistical', function () {
             assert.sameMembers(res.map(v => Math.trunc(v * 100) / 100), [0.66, 2.66]);
         });
     });
+
+    describe('should compute standard deviation', () => {
+        it('of an array of numbers', () => {
+            const data = {set1: [1, 2, 3]};
+            /* Trunc result and keep two decimals without rounded to keep period */
+            const res = Math.trunc(statistical.stdDeviation(data) * 100) / 100;
+
+            assert.equal(res, 0.81);
+        });
+
+        it('of multiple array of numbers', () => {
+            const data = {
+                set1: [1, 2, 3],
+                set2: [1, 3, 5]
+            };
+            /* Trunc result and keep two decimals without rounded to keep period */
+            const res = statistical.stdDeviation(data).map(v => Math.trunc(v * 100) / 100);
+
+            assert.equal(Array.isArray(res), true);
+            assert.sameMembers(res.map(v => Math.trunc(v * 100) / 100), [0.81, 1.63]);
+        });
+    });
 });
