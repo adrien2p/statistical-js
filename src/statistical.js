0c63990565c7af1817282d40fa0801a6fa07b6ae
AdrienDePeretti <adrien.deperetti.freelance@gmail.com>
2017-01-27 23:05:36 +0100

    Add coverage support throught coveralls.io, update unit test and readme

--- src/statistical.js
+++ src/statistical.js
@@ -5,24 +5,64 @@ class Statistical {
     constructor() {}
 
     /**
-     * Take array and return sum of each elements
+     * Take array and return sum of each elements.
      *
      * @param {Array} set
      * @returns {*}
      */
     sum(set) {
-        return set.reduce((a, b) => {
-            if (Number.isNaN(a) || Number.isNaN(b)) throw new Error('dataSet must contain only numbers');
-            return a + b;
+        return set.reduce((res, val) => {
+            if (Number.isNaN(val)) throw new Error('dataSet must contain only numbers');
+            return res + val;
         }, 0);
     }
 
+    /**
+     * Compute median for dataSet, the central value.
+     *
+     * @param {Array} set
+     * @returns {number}
+     */
     median(set) {
+        if (!set) throw new Error('Missing parameter set (median)');
+        if (!Array.isArray(set)) throw new Error('dataSet must be an array (median)');
 
+        const middle = Math.floor(set.length / 2);
+        const isEven = set.length % 2 === 0;
+
+        set.sort((a, b) => a - b);
+
+        return isEven ? (set[middle - 1] + set[middle]) / 2 : set[middle];
     }
 
     /**
-     * Compute average for dataSet
+     * Get the value wit the miximum occurence.
+     *
+     * @param {Array} set
+     * @returns {*}
+     */
+    mode(set) {
+        const counter = {};
+        let mode = [];
+        let max = 0;
+
+        for (const index in set) {
+            if (!(set[index] in counter)) counter[set[index]] = 0;
+
+            counter[set[index]]++;
+
+            if (counter[set[index]] === max) mode.push(set[index]);
+            if (counter[set[index]] > max) {
+                max = counter[set[index]];
+                mode = [set[index]];
+            }
+        }
+
+        return mode;
+    }
+
+    /**
+     * Compute average for dataSet.
      *
      * @param {Array} set
      * @returns {*}
@@ -31,11 +71,11 @@ class Statistical {
         if (!set) throw new Error('Missing parameter set (average)');
         if (!Array.isArray(set)) throw new Error('dataSet must be an array (average)');
 
-        return this.sum(set) / set.length;;
+        return this.sum(set) / set.length;
     }
 
     /**
-     * Compute variance for dataSet
+     * Compute variance for dataSet.
      *
      * @param {Array} set
      * @returns {*}
@@ -51,7 +91,7 @@ class Statistical {
     }
 
     /**
-     * Compute standard deviation for dataSet
+     * Compute standard deviation for dataSet.
      *
      * @param {Array} set
      * @returns {*}
--- test/statistical.spec.js
+++ test/statistical.spec.js
@@ -1,41 +1,67 @@
 const assert = require('chai').assert;
 const statistical = require('../src/statistical');
 
-describe('Statistical', function () {
-    describe('should compute sum', () => {
-        it('of an array of numbers', () => {
-            const res = statistical.sum([1, 2, 3]);
-
-            assert.equal(res, 6);
-        });
+describe('Statistical class', function () {
+    it('should compute sum of an array of numbers', () => {
+        const res = statistical.sum([1, 2, 3]);
+        assert.equal(res, 6);
     });
 
-    describe('should compute average', () => {
-        it('of an array of numbers', () => {
-            const data = [1, 2, 3];
-            const res = statistical.average(data);
+    it('should return the central value of an array of numbers (even)', () => {
+        const data = [1, 2, 3, 4];
+        const res = statistical.median(data);
 
-            assert.equal(res, 2);
-        });
+        assert.equal(res, 2.5);
     });
 
-    describe('should compute variance', () => {
-        it('of an array of numbers', () => {
-            const data = [1, 2, 3];
-            /* Trunc result and keep two decimals without rounded to keep period */
-            const res = Math.trunc(statistical.variance(data) * 100) / 100;
+    it('should return the median of an array of numbers (odd)', () => {
+        const data = [1, 2, 3];
+        const res = statistical.median(data);
 
-            assert.equal(res, 0.66);
-        });
+        assert.equal(res, 2);
     });
 
-    describe('should compute standard deviation', () => {
-        it('of an array of numbers', () => {
-            const data = [1, 2, 3];
-            /* Trunc result and keep two decimals without rounded to keep period */
-            const res = Math.trunc(statistical.stdDeviation(data) * 100) / 100;
+    it('should return the median of an array of numbers (odd)', () => {
+        const data = [1, 2, 3];
+        const res = statistical.median(data);
 
-            assert.equal(res, 0.81);
-        });
+        assert.equal(res, 2);
+    });
+
+    it('should return the mode of an array of numbers', () => {
+        const data = [1, 2, 3, 3, 3, 1, 1, 1, 1];
+        const res = statistical.mode(data);
+
+        assert.equal(res, 1);
+    });
+
+    it('should return multiple mode value of an array of numbers', () => {
+        const data = [1, 2, 3, 3, 3, 3, 3, 1, 1, 1, 1];
+        const res = statistical.mode(data);
+
+        assert.sameMembers(res, [1, 3]);
+    });
+
+    it('should compute average of an array of numbers', () => {
+        const data = [1, 2, 3];
+        const res = statistical.average(data);
+
+        assert.equal(res, 2);
+    });
+
+    it('should compute variance of an array of numbers', () => {
+        const data = [1, 2, 3];
+        /* Trunc result and keep two decimals without rounded to keep periodicity */
+        const res = Math.trunc(statistical.variance(data) * 100) / 100;
+
+        assert.equal(res, 0.66);
+    });
+
+    it('should compute standard deviation of an array of numbers', () => {
+        const data = [1, 2, 3];
+        /* Trunc result and keep two decimals without rounded to keep periodicity */
+        const res = Math.trunc(statistical.stdDeviation(data) * 100) / 100;
+
+        assert.equal(res, 0.81);
     });
 });
