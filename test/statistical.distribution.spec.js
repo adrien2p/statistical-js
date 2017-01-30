a96b10ee2027954ebea847aeebd3690c6c0e5701
AdrienDePeretti <adrien.deperetti.freelance@gmail.com>
2017-01-30 23:00:38 +0100

    update test

--- test/statistical.distribution.spec.js
+++ test/statistical.distribution.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('chai').assert;
-const StatisticalDistribution = require('../src/statistical.distribution');
+const StatisticalDistribution = require('../src/statistical').distribution;
 
 describe('StatisticalDistribution', () => {
     let statisticalDistribution;
@@ -9,7 +9,7 @@ describe('StatisticalDistribution', () => {
 
     it('should return binomial probability result for each value', () => {
         const res = statisticalDistribution.binomial(10, 0.5);
-        
+
         assert.equal(Object.keys(res).length === 11, true);
         Object.keys(res).forEach(k => {
             assert.equal(res[k] > 0 && res[k] < 1, true);
